c445d4815ae5329ec0ec962a2e8a8382bb0ca2ec
Robert Zlomke <rzlomke@stud.hs-heilbronn.de>
2020-11-12 09:55:31 +0100

    Added FollowPlayer and RandomMovement Scripts

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,19 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+    private NavMeshAgent agent;
+    Animator animator;
+    public ThirdPersonCharacter character;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        if (animator)
+        {
+            agent.updateRotation = false;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         GetComponent<NavMeshAgent>().SetDestination(player.position);
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (animator != null)
+        {
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+
+public class RandomMovement : MonoBehaviour
+{
+    public float wanderRadius;
+    public float wanderTimer;
+
+    private Transform target;
+    private NavMeshAgent agent;
+    private float timer;
+    Animator animator;
+    public ThirdPersonCharacter character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        if (animator)
+        {
+            agent.updateRotation = false;
+            
+        }
+    }
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+        //animator.SetFloat("Forward", agent.desiredVelocity.magnitude);
+
+        Movement();
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+
+    void Movement()
+    {
+        if (animator != null)
+        {
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+    }
+}
